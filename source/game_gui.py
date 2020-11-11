55bf496e2a30bc48ffe17fec2f05f5761fef838e
nelson-bht <s81071@beuth-hochschule.de>
2020-11-11 18:52:16 +0100

    new simple gui

--- source/game_gui.py
+++ source/game_gui.py
@@ -2,57 +2,53 @@
 ''' Graphical User Interface for EduQuiz'''
 import tkinter as tk
 
-class TopGui(tk.Frame):
-
-    def __init__(self):
-        master = tk.Tk()
-        super().__init__(master)
-        #self.master = master
-
+class Gui:
+    _top = tk.Tk()
+    _top.resizable(0,0)
+    _quiz_frame = tk.Frame(_top, bg='grey')
+    _edit_frame = tk.Frame(_top, bg='blue')
+    @classmethod
+    def run(cls):
+        # Two Frames
+        EDIT = cls._edit_frame
+        QUIZ = cls._quiz_frame
         
-    def run(self):
-        #init Menu
-        self.master.resizable(0,0)
-        self.master.title('Menu')
-        self.buttonGame = tk.Button(self.master, text='Start Quiz', width=20,
-                                    height = 10, bg='orange',activebackground ='olivedrab2',
-                                    bd = 4, font = ('arial','10','bold'))
-        self.buttonGame.pack(side=tk.LEFT)
+        # Init Quiz Frame
+        QUIZ.pack(side=tk.LEFT)
+        var_quiz_label = tk.StringVar()
+        quiz_label = tk.Label(QUIZ, textvariable=var_quiz_label)
+        var_quiz_label.set('Question')
+        quiz_label.pack(side=tk.RIGHT, fill=tk.X)
         
-        self.buttonEditor = tk.Button(self.master, text='Enter Editor', width=20,
-                                      height = 10, bg='olivedrab1', activebackground ='olivedrab2',
-                                      bd = 4, font = ('arial','10','bold'))
-        self.buttonEditor.pack(side=tk.RIGHT)
-        self.mainloop()
+        # Init Editor Frame
+        EDIT.pack(side=tk.RIGHT, fill=tk.X)
+        var_edit_label = tk.StringVar()
+        edit_label = tk.Label(EDIT, textvariable=var_edit_label)
+        var_edit_label.set('Editor')
+        edit_label.pack(side=tk.TOP, fill=tk.X)
+        # new question
+        new_question_label = tk.Label(EDIT, text='Enter question text below:')
+        new_question_label.pack(side=tk.TOP, fill=tk.X)
+        new_question_entry = tk.Text(EDIT, font=5, width= 25, height=5)
+        new_question_entry.pack(side=tk.TOP,fill=tk.X)
         
+        new_answer_label = tk.Label(EDIT, text='Enter correct answer text below:')
+        new_answer_label.pack(side=tk.TOP, fill=tk.X)
+        new_answer_entry = tk.Entry(EDIT, font=5, width= 25)
+        new_answer_entry.pack(side=tk.TOP,fill=tk.X)
 
-class Editor(tk.Frame):
-    def __init__(self):
-        master = tk.Tk()
-        super().__init__(master)
+        new_wrong1_label = tk.Label(EDIT, text='Enter first wrong answer text below:')
+        new_wrong1_label.pack(side=tk.TOP, fill=tk.X)
+        new_wrong1_entry = tk.Entry(EDIT, font=5, width= 25)
+        new_wrong1_entry.pack(side=tk.TOP,fill=tk.X)
 
-    def save_question(self):
-        self.master.destroy()
-        print('start quiz\nclose menu')
+        new_wrong2_label = tk.Label(EDIT, text='Enter second wrong answer text below:')
+        new_wrong2_label.pack(side=tk.TOP, fill=tk.X)
+        new_wrong2_entry = tk.Entry(EDIT, font=5, width= 25)
+        new_wrong2_entry.pack(side=tk.TOP,fill=tk.X)
         
-        
-    def run(self):
-        #init Menu
-        self.master.resizable(0,0)
-        self.master.title('Menu')
-        self.button_save_q = tk.Button(self.master, text='Save Question',
-                                    width=20, height = 10, command=self.save_question,
-                                    bg='orange',activebackground ='olivedrab2',
-                                    bd = 4, font = ('arial','10','bold'))
-        self.button_exit = tk.Button(self.master, text='Save Question',
-                                    width=20, height = 10, command=self.master.destroy,
-                                    bg='orange',activebackground ='olivedrab2',
-                                    bd = 4, font = ('arial','10','bold'))
-        self.button_exit.pack(side=tk.LEFT)
-        self.mainloop()
-# class GameGui:
+        button_submit_question = tk.Button(EDIT, text='Save Question', bg='grey')
+        button_submit_question.pack(side=tk.BOTTOM, fill=tk.X)
+        cls._top.mainloop()
 
-top = TopGui()
-top.run()
-edit = Editor()
-edit.run()
+Gui.run()
